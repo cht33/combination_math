c1cab31688c86f51e63e7b1e49d8c86007636165
Neptune <1115876559@qq.com>
2021-10-12 00:11:50 +0800

    添加了项目报告文件； 完成了递增，递减，字典序，新递归的报告书写； 上述函数的注释添加，和部分可读性优化；

--- main.cpp
+++ main.cpp
@@ -4,74 +4,86 @@
 #include <iostream>
 #include <ctime>
 
-// #define SHOW
+#define SHOW
 
 struct Permutation {
     const static int MAXN = 1000;
     int N;
-    char s[2*MAXN]; // output string
-    char s1[2*MAXN]; // backup
-    int lb;
+    int mediation[MAXN]; // 中介数
+    char s[2*MAXN]; // 构造的排列
     char* st;
-    int r[MAXN]; // radix number
     int cnt;
-    int next[MAXN];
+    int next[MAXN]; // 递增和递减中，按定义用中介数生成排列用的数组
+    int mark[MAXN]; // 递归算法中，标记特定位已经被使用过
+    int boundary;
+    char useable_chars[2*MAXN];
 
     void init() {
         memset(s, 0, 2*MAXN*sizeof(char));
-        memset(r, 0, MAXN*sizeof(int));
+        memset(mediation, 0, MAXN*sizeof(int));
         for (int i = 1; i <= N; i++) {
             s[i] = 'a' + i - 1;
-            s1[i-1] = 'a' + i - 1;
+            useable_chars[i] = 'a' + i - 1;
         }
         s[0] = 0;
         cnt = 1;
-        lb = N;
+        boundary = N;
         st = s+1;
     }
 
     void generate(int n) {
         N = n;
         auto start = clock();
+
+        init();
+        start = clock();
+        next_permutation();
+        printf("STL next_permutation: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        init();
+        start = clock();
+        recursive();
+        printf("recursive: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        init();
+        start = clock();
+        increase_base();
+        printf("increase_base: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        init();
+        start = clock();
+        optimized_increase_base();
+        printf("optimized_increase_base: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        init();
+        start = clock();
+        decrease_base();
+        printf("decrease_base: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        init();
+        start = clock();
+        optimized_decrease_base();
+        printf("optimized_decrease_base: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        // init();
+        // start = clock();
+        // neighbour_exchange();
+        // printf("neighbour_exchange: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
         init();
         start = clock();
         dictionary();
-        printf("dictionary: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+        printf("dictionary: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+
+        // init();
+        // start = clock();
+        // heap();
+        // printf("heap: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
 
         init();
         start = clock();
-        heap();
-        printf("heap: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        // init();
-        // start = clock();
-        // new_recursive();
-        // printf("new_recursive: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        // init();
-        // start = clock();
-        // next_permutation();
-        // printf("next_permutation: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        // init();
-        // start = clock();
-        // increase_base();
-        // printf("increase_base: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        // init();
-        // start = clock();
-        // new_increase_base();
-        // printf("new_increase_base: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        // init();
-        // start = clock();
-        // decrease_base();
-        // printf("decrease_base: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
-
-        init();
-        start = clock();
-        neighbour_exchange();
-        printf("neighbour_exchange: %lf\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
+        new_recursive();
+        printf("new_recursive: %.2lfms\n", (double)(clock() - start)*1000/CLOCKS_PER_SEC);
     }
 
     void heap() {
@@ -80,69 +92,78 @@ struct Permutation {
         #endif
         int i = 1;
         while (i < N) {
-            if (r[i] < i) {
-                int t = (i & 1) ? r[i] : 0;
+            if (mediation[i] < i) {
+                int t = (i & 1) ? mediation[i] : 0;
                 char c = st[t]; st[t] = st[i], st[i] = c;
                 #ifdef SHOW
                 puts(st);
                 #endif
-                r[i]++, i = 1;
+                mediation[i]++, i = 1;
             } else {
-                r[i++] = 0;
+                mediation[i++] = 0;
             }
         }
     }
 
     void increase_base() {
         while(true){
-            for(int i = 0;i <= N;i ++)next[i] = i - 1;
-            for(int i = N - 2;i >= 0;i --){
-                int itemcnt = r[i];
-                int preindex = N;
-                int index = next[N];
-                while(itemcnt){
+            
+            // 初始化链表结构
+            for(int i = 1;i <= N + 1;i ++)next[i] = i - 1;
+
+            // 按照定义把中介数转为对应的排列
+            for(int i = N;i >= 2;i --){
+                int emptycnt = mediation[i];
+                int preindex = N + 1;
+                int index = next[N + 1];
+                while(emptycnt){
                     preindex = index;
                     index = next[index];
-                    itemcnt --;
+                    emptycnt --;
                 }
-                s[index + 1] = 'a' + i + 1;
+                s[index] = 'a' + i - 1;
                 next[preindex] = next[index];
             }
-            s[next[N] + 1] = 'a';
+            s[next[N + 1]] = 'a';
 
             #ifdef SHOW
             puts(s+1);
             #endif
 
-            int i = 0;
-            r[i] ++;
-            while(r[i] >= i + 2){
-                r[i] -= i + 2;
-                r[++i] ++;
+            // 递增进制数加一
+            int i = 2;
+            mediation[i] ++;
+            while(mediation[i] >= i){
+                mediation[i] -= i;
+                mediation[++ i] ++;
             }
-            if(r[N - 1])break;
+            if(i == N + 1)break;
         }
     }
 
-    void new_increase_base() {
+    void optimized_increase_base() {
+        
+        // 初始化位置数组，pointer[x]表示x当前在排列中的位置
         int pointer[MAXN];
         for(int i = 1;i <= N;i ++){
             pointer[i] = i;
         }
         while(true) {
+            
             #ifdef SHOW
             puts(s+1);
             #endif
 
+            // 递增进制数加一
             int i = 2;
-            r[i] ++;
-            while(r[i] >= i){
-                r[i] -= i;
-                r[++i] ++;
+            mediation[i] ++;
+            while(mediation[i] >= i){
+                mediation[i] -= i;
+                mediation[++ i] ++;
             }
-
             if(i == N + 1)break;
 
+            // 将最大的k在1-k的子排列中前移一位
             for(int j = 1;j < i; j ++) {
                 int pj = pointer[j], pi = pointer[i];
                 if (pj < pi) {
@@ -151,6 +172,8 @@ struct Permutation {
                     break;
                 }
             }
+
+            // 翻转1-(k-1)，进位前他们必然是逆序
             int l = 1, r = i - 1;
             while(l < r){
                 int pl = pointer[l], pr = pointer[r];
@@ -162,31 +185,75 @@ struct Permutation {
     }
 
     void decrease_base() {
+
+        while(true){
+            
+            // 初始化链表结构
+            for(int i = 1;i <= N + 1;i ++)next[i] = i - 1;
+
+            // 按照定义把中介数转为对应的排列
+            for(int i = N;i >= 2;i --){
+                int emptycnt = mediation[i];
+                int preindex = N + 1;
+                int index = next[N + 1];
+                while(emptycnt){
+                    preindex = index;
+                    index = next[index];
+                    emptycnt --;
+                }
+                s[index] = 'a' + i - 1;
+                next[preindex] = next[index];
+            }
+            s[next[N + 1]] = 'a';
+            
+            #ifdef SHOW
+            puts(s + 1);
+            #endif
+
+            // 递减进制数加一
+            int i = N;
+            mediation[i] ++;
+            while (mediation[i] == i) {
+                mediation[i] = 0;
+                mediation[-- i] += 1;
+            }
+            if (i == 0) break;
+        }
+
+    }
+
+    void optimized_decrease_base() {
         auto st = s+1;
         int idx_N = N-1;
         while (true) {
+            
             #ifdef SHOW
             puts(st);
             #endif
 
+            // 递减进制数加一(包含最低位的快速计算)
             int i = N;
-            r[i] += 1;
-            if (r[i] < N) {
+            mediation[i] += 1;
+            if (mediation[i] < N) {
                 char c = st[idx_N];
                 idx_N -= 1;
                 st[idx_N+1] = st[idx_N];
                 st[idx_N] = c;
                 continue;
             }
-            while (r[i] == i) {
-                r[i] = 0;
-                r[--i] += 1;
+            while (mediation[i] == i) {
+                mediation[i] = 0;
+                mediation[-- i] += 1;
             }
             if (i == 0) break;
+
+            // 将最小的k在1-k的子排列中前移一位
             int j = 0;
             char c = 'a'+i-1;
             while (st[j] != c) j++;
             c = st[j], st[j] = st[j-1], st[j-1] = c;
+            
+            // 将位于高位的逆序n,n-1,..,n-k+1移动到低位并翻转
             int t = N-i;
             for (j = 0; j < t; j++) {
                 st[N+j] = st[t-1-j];
@@ -209,27 +276,27 @@ struct Permutation {
             #endif
 
             int i = N;
-            r[i] += 1;
-            if (r[i] < N) {
+            mediation[i] += 1;
+            if (mediation[i] < N) {
                 char c = s[idx_N];
                 idx_N += t_N;
                 s[idx_N-t_N] = s[idx_N];
                 s[idx_N] = c;
                 continue;
             }
-            while (r[i] == i) {
-                r[i] = 0;
-                r[--i] += 1;
+            while (mediation[i] == i) {
+                mediation[i] = 0;
+                mediation[--i] += 1;
             }
             if (i == 0) break;
             int j = 1;
             char c = 'a'+i-1;
             while (s[j] != c) j++;
             // 只需考虑最后一位的运算结果，1为奇，0为偶
-            int t = (r[i-1] ^ (~i & r[i-2])) & 1;
+            int t = (mediation[i-1] ^ (~i & mediation[i-2])) & 1;
             t = (t << 1) - 1;
             c = s[j+t], s[j+t] = s[j], s[j] = c;
-            t_N = (r[N-1] ^ (~N & r[N-2])) & 1;
+            t_N = (mediation[N-1] ^ (~N & mediation[N-2])) & 1;
             t_N = (t_N << 1) - 1;
         }
     }
@@ -245,14 +312,9 @@ struct Permutation {
 
     void recursive() {
         for (int i = 1; i <= N; i++) {
-            if (r[i] != 0) continue;
-            r[i] = 1;
-
-            #ifdef SHOW
+            if (mark[i] != 0) continue;
+            mark[i] = 1;
             s[cnt++] = 'a' + i - 1;
-            #else
-            s[cnt++] = i;
-            #endif
 
             if (cnt == N+1) {
                 #ifdef SHOW
@@ -261,63 +323,75 @@ struct Permutation {
             } else {
                 recursive();
             }
-            r[i] = 0;
+            mark[i] = 0;
             cnt--;
         }
     }
 
     void new_recursive() {
-        if (lb == 0) {
+        if (boundary == 0) {
             #ifdef SHOW
             puts(s+1);
             #endif
             return;
         }
-        for (int i = 0; i < lb; i++) {
-            s[cnt++] = s1[i];
-            std::swap(s1[i], s1[--lb]);
+        // 每层迭代只能在可用集中探索
+        for (int i = 1; i <= boundary; i++) {
+            // 使用了可用集第i位元素
+            s[cnt++] = useable_chars[i];
+            // 维护可用集，可用集大小减一，第i位元素出集
+            std::swap(useable_chars[i], useable_chars[boundary--]);
             new_recursive();
-            std::swap(s1[i], s1[lb++]);
+            // 维护可用集，可用集大小加一，第i位元素入集
+            std::swap(useable_chars[i], useable_chars[++boundary]);
             cnt--;
         }
     }
 
-    void increase_base_recursive() {
-        for (int i = N; i > 0; i--) {
-            if (r[i] != 0) continue;
-            r[i] = 1;
+    // void increase_base_recursive() {
+    //     for (int i = N; i > 0; i--) {
+    //         if (mediation[i] != 0) continue;
+    //         mediation[i] = 1;
 
-            #ifdef SHOW
-            s[i] = 'a' + N - cnt++;
-            #else
-            s[cnt++] = i;
-            #endif
+    //         #ifdef SHOW
+    //         s[i] = 'a' + N - cnt++;
+    //         #else
+    //         s[cnt++] = i;
+    //         #endif
 
-            if (cnt == N+1) {
-                #ifdef SHOW
-                puts(s+1);
-                #endif
-            } else {
-                increase_base_recursive();
-            }
-            r[i] = 0;
-            cnt--;
-        }
-    }
+    //         if (cnt == N+1) {
+    //             #ifdef SHOW
+    //             puts(s+1);
+    //             #endif
+    //         } else {
+    //             increase_base_recursive();
+    //         }
+    //         mediation[i] = 0;
+    //         cnt--;
+    //     }
+    // }
 
     void dictionary() {
         while (true) {
+            
             #ifdef SHOW
             puts(s+1);
             #endif
 
+            // 首先找到第一个高位数值小于低位数值的点
             int i = N-1;
             while (s[i+1] < s[i]) i--;
             if (i == 0) break;
+            
+            // 再找到尾部序列中，大于这个高位数值的最小值
             int j = N;
             while (s[j] < s[i]) j--;
+            
+            // 交换二者
             char c = s[j];
             s[j] = s[i], s[i] = c;
+            
+            // 将剩余的序列翻转
             // std::reverse(s+i+1, s+N+1);
             auto l = s+i+1, r = s+N;
             while (l < r)
